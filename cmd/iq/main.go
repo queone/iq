3eb3a81f827ea4358eb4f4374fc7fa8474f44671
Lenny Capellan <104685438+kquo@users.noreply.github.com>
2026-09-17 12:17:51 -0400

    AC25 adopt Govna governance files v0.63.0

--- cmd/iq/main.go
+++ cmd/iq/main.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	programName    = "iq"
-	programVersion = "0.19.3"
+	programVersion = "0.19.4"
 	programURL     = "iq"
 	programSummary = "Work with IQ from the command line"
 )
